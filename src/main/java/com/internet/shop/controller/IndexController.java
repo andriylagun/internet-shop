1ee7298aeff001761254a2d917cfd8d6b26d0dbb
Andriy <andriy.lagun@gmail.com>
2020-09-08 16:00:18 +0300

    Added empty line into web.xml.

--- src/main/java/com/internet/shop/controller/IndexController.java
+++ src/main/java/com/internet/shop/controller/IndexController.java
@@ -2,10 +2,12 @@ package com.internet.shop.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet({"/","/home"})
 public class IndexController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
