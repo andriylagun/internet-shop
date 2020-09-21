f4c9e80c5a775b663efe1e49702cce6c9518b7fb
Andriy <andriy.lagun@gmail.com>
2020-09-21 09:35:38 +0300

    Added allargsconstructor

--- src/main/java/com/internet/shop/model/Product.java
+++ src/main/java/com/internet/shop/model/Product.java
@@ -1,15 +1,16 @@
 package com.internet.shop.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Product {
     private Long id;
     private String name;
     private double price;
 
-    public Product(Long id, String name, double price) {
-        this.id = id;
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
